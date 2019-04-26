ff2c1bb91f6f940f58578f215f364e1ab1010a66
JacquelineChao <48360711+JacquelineChao@users.noreply.github.com>
2019-04-26 11:53:00 -0400

    Sizing Images Correctly

--- 5.2.3/App.js
+++ 5.2.3/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, View, StyleSheet, Image } from 'react-native';
+import { Constants } from 'expo';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: 'https://static.codehs.com/img/courses/introkarel/1.png' }}
+                    style={{ height: 125, width: 200 }}
+                />
+                <Image
+                    source={{ uri: 'https://static.codehs.com/img/courses/introkarel/2.png' }}
+                    style={{ height: 200, width: 125 }}
+                />
+                <Image
+                    source={{ uri: 'https://static.codehs.com/img/courses/introkarel/3.png' }}
+                    style={{ height: 200, width: 200 }}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
