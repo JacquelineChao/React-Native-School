c2766d46f58ea244a8f8c00ddcf5f23d283c6bdd
JacquelineChao <48360711+JacquelineChao@users.noreply.github.com>
2019-04-26 11:59:45 -0400

    Setting a Background Image with Flex

--- 5.2.5/App.js
+++ 5.2.5/App.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, View, StyleSheet, Text, ImageBackground } from 'react-native';
+import { Constants } from 'expo';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground
+                    style={styles.background}
+                    source={{ uri: 'https://c1.staticflickr.com/6/5253/5426098861_4aee73fb6d_b.jpg' }}
+                >
+                    <View style={styles.textContainer}>
+                        <Text style={styles.paragraph}>
+                            Hello, Universe!
+                        </Text>
+                    </View>
+                </ImageBackground>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
+    background: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    paragraph: {
+        textAlign: 'center',
+        fontSize: 50,
+        fontFamily: 'Courier New',
+    }
 });
